5e0f49fc0ba4da186388e3e757c28f09a6927c33
priyanka verma <pv209304@gmail>
2024-07-03 00:20:57 +0530

    Initial commit

---
+++ src/NewOne.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class NewOne extends React.Component{
+    render(){
+        return(
+            <h1>HEY THIS IS NewOne</h1>
+        )
+    }
+
+}
+export default NewOne;
